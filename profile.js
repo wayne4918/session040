8e4bd45fd8df2a2554d4ebb4ac64b8d436233b69
ryu <ryudonghyun4918@naver.com>
2024-04-10 22:45:25 +0900

    test commit

--- profile.js
+++ profile.js
@@ -1,13 +1,13 @@
 // script.js
 document.getElementById("introductionBtn").addEventListener("click", function() {
-    window.location.href = "자기소개페이지.html";
+    window.location.href = "INTRODUCE.html";
   });
   
   document.getElementById("infoBtn").addEventListener("click", function() {
-    window.location.href = "내정보페이지.html";
+    window.location.href = "Ryu's TMI.html";
   });
   
   document.getElementById("plansBtn").addEventListener("click", function() {
-    window.location.href = "앞으로의계획페이지.html";
+    window.location.href = "Plan.html";
   });
   
